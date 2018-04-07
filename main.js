b40418b144980308faa6b45a87a1ac0c10a96f22
Magi-03 <Relmnight@gmail.com>
2018-04-07 06:24:42 +0200

    just fucking around

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ $.ajax("info.txt").done(function(data) {
 .fail(function() {
     alert("could not get data");
   });
+
+function isEmail(email) {
+   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+   return regex.test(email);
+} 
+
+$("#submit").click(function() {
+    alert(isEmail($("#email").val()));
+  });  
+
+  $("#btn").click(function() {
+    alert("oh wee");
+  });  
