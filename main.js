e31d1cedc4e44f831907c128f692c02a4e36840d
Magi-03 <Relmnight@gmail.com>
2018-04-09 23:15:01 +0200

    https?

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $("#btnSearch").click(function () {
         var searchResult = $("#usr").val();
-        apiurl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=b34df3818e00c38fd96d86faf15835d4"
+        apiurl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=b34df3818e00c38fd96d86faf15835d4"
         var promised = promiseAjax();
         promised.done(function (data) {
             console.log(data)
