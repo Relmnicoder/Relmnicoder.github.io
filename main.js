f16d44c393a820ee91a950bf588e602a06a95744
Magi-03 <Relmnight@gmail.com>
2018-04-09 14:24:14 +0200

    more ajax

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-$("#btnSearch").click(function () {
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b34df3818e00c38fd96d86faf15835d4",
-        success: function (data) {
-            console.log(data)
-          }
-    })
-  });
+
+$(document).ready(function () {
+    $("#btnSearch").click(function () {
+        var searchResult = $("#usr").val();
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "http://samples.openweathermap.org/data/2.5/weather?q="+searchResult+",uk&appid=b34df3818e00c38fd96d86faf15835d4",
+            success: function (data) {
+                console.log(data)
+            }
+        })
+    });
+});
