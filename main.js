652a93ea1b16d22c1854cb15fcd537b020915410
Magi-03 <Relmnight@gmail.com>
2018-04-09 14:58:55 +0200

    Should work, but doenst. why?

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 $(document).ready(function () {
     $("#btnSearch").click(function () {
         var searchResult = $("#usr").val();
+        apiurl = "api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=b34df3818e00c38fd96d86faf15835d4"
         $.ajax({
             type: "GET",
             dataType: "json",
-            url: "http://samples.openweathermap.org/data/2.5/weather?q="+searchResult+",uk&appid=b34df3818e00c38fd96d86faf15835d4",
+            url: apiurl,
             success: function (data) {
                 console.log(data)
             }
