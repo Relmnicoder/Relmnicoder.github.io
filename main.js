73b26236d8101d3141fe28eb4d4f79c745294f0f
Magi-03 <Relmnight@gmail.com>
2018-04-09 23:12:13 +0200

    I think I used a defferred. Well i guess just a promise? Not really sure

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ $(document).ready(function () {
     $("#btnSearch").click(function () {
         var searchResult = $("#usr").val();
         apiurl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=b34df3818e00c38fd96d86faf15835d4"
-        $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: apiurl,
-            success: function (data) {
-                console.log(data)
-            }
+        var promised = promiseAjax();
+        promised.done(function (data) {
+            console.log(data)
         })
-    });
+    })
 });
+function promiseAjax() {
+    return $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: apiurl,
+    })
+}
