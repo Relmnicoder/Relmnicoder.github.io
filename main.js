ce050ca01fd15f8150a90aaa9c4f07f1a88713b7
Magi-03 <Relmnight@gmail.com>
2018-04-12 01:40:55 +0200

    doesnt matter

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
-
 $(document).ready(function () {
     $("#btnSearch").click(function () {
-        var searchResult = $("#usr").val();
-        apiurl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=b34df3818e00c38fd96d86faf15835d4"
-        var promised = promiseAjax();
-        promised.done(function (data) {
-            console.log(data)
-        })
-        promised.fail(function (data) {
-            alert("Problem with connecting to API")
-          })
+       
+        var url = $("#textInput").val();
+        todayWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + url + "&appid=ee595a2e51b9aa75efcfd4297fb55b8c";
+        forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + url + "&appid=ee595a2e51b9aa75efcfd4297fb55b8c";
+
+
+        $.when($.getJSON(todayWeatherUrl), $.getJSON(forecastUrl))
+            .done(function (a1, a2, ) {
+
+                var aktTempRound = Math.round(a1[0].main.temp - 273.15);
+                $("#aktStand").html("<td scope='row'> " + aktTempRound + "°C</td>");
+
+                var firstDay = 0;
+                var secondDay = 0;
+                var thirdDay = 0;
+                var fourthDay = 0;
+                var fifthDay = 0;
+                var avgDays = 0;
+
+                for (i = 0; i < (a2[0].list).length; i++) {
+                    if (i < 8) {
+                        firstDay += a2[0].list[i].main.temp;
+                    }
+                    else if (i > 7 && i < 16) {
+                        secondDay += a2[0].list[i].main.temp;
+                    }
+                    else if (i > 15 && i < 24) {
+                        thirdDay += a2[0].list[i].main.temp;
+                    }
+                    else if (i > 23 && i < 32) {
+                        fourthDay += a2[0].list[i].main.temp;
+                    }
+                    else {
+                        fifthDay += a2[0].list[i].main.temp;
+                    }
+                    avgDays += a2[0].list[i].main.temp;
+                }
+                firstDay = Math.round((firstDay / 8) - 273.15);
+                secondDay = Math.round((secondDay / 8) - 273.15);
+                thirdDay = Math.round((thirdDay / 8) - 273.15);
+                fourthDay = Math.round((fourthDay / 8) - 273.15);
+                fifthDay = Math.round((fifthDay / 8) - 273.15);
+
+                var tage = [firstDay, secondDay, thirdDay, fourthDay, fifthDay];
+
+                for (i = 0; i < tage.length; i++) {
+                    $("#row" + (i + 1)).html("<td scope='row'> " + tage[i] + "°C</td>");
+                }
+
+                for (i = 0; i < tage.length; i++) {
+                    if (aktTempRound < tage[i]) {
+                        $("#row"+(i + 1)).removeClass("getsColder");
+                        $("#row"+(i + 1)).addClass("getsWarmer");
+                    }
+                    else if (aktTempRound > tage[i]) {
+                        $("#row"+(i + 1)).removeClass("getsWarmer");
+                        $("#row"+(i + 1)).addClass("getsColder");
+                    }
+                    else {
+                        $("#row"+(i + 1)).removeClass("getsWarmer");
+                        $("#row"+(i + 1)).removeClass("getsColder");
+                    }
+                }
+            })
+
     })
-});
-function promiseAjax() {
-    return $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: apiurl,
-    })
-}
+
+})
